ee4c81800759b2771ed50ba06692e6902898b60e
hyb1996 <946994919@qq.com>
2017-07-05 15:52:41 +0800

    save works before fixing bugs

--- app/src/test/java/com/stardust/scriptdroid/ExampleUnitTest.java
+++ app/src/test/java/com/stardust/scriptdroid/ExampleUnitTest.java
@@ -32,13 +32,20 @@ import static org.junit.Assert.assertTrue;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    // TODO: 2017/7/2 卡片式脚本
-    // TODO: 2017/7/2 底部导航栏
+    //// TODO: 2017/7/4 恢复项目 一天 7.6
+    // TODO: 2017/7/4  修复 一天 7.7
+    // TODO: 2017/7/4 修复Bug，增加一点脚本，发布2.0.13Beta2 半天 7.8
+    // TODO: 2017/7/2 底部导航栏 一天 7.9
+    //// TODO: 2017/7/4 教程，服务器界面 二至三天 7.12
+    // TODO: 2017/7/4  服务器初步开发 三天 7.15
+    // TODO: 2017/7/4  细节 三天 7.18
+    // TODO: 2017/7/4 写文档 两天 7.20
+    // TODO: 2017/7/4 发布 3.0.0 Beta 7.22
 
 
     @Test
     public void test() {
-
+        System.out.println("SOS".hashCode());
     }
 
     @Test
