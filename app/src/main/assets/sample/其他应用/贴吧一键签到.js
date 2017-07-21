ab2ad906e767f188c70c33e22224104b5c2c0135
hyb1996 <946994919@qq.com>
2017-07-21 12:55:33 +0800

    fix and add some scripts

--- app/src/main/assets/sample/其他应用/贴吧一键签到.js
+++ app/src/main/assets/sample/其他应用/贴吧一键签到.js
@@ -2,39 +2,48 @@
 
 launchApp("百度贴吧");
 sleep(1000);
-while(!click("进吧"));
+id("tabcontainer").untilFind().each(function(tab){
+  if(tab.childCount() != 5){
+    return;
+  }
+  var ba = tab.child(1);
+  if(ba){
+    ba.click();
+  }
+});
 toast("开始签到");
 var signed = {};
 
 while(true){
-    var list = id("fragment_pager").findOne().findOne(id("listview"));
+    var list = id("tab_content").findOne().findOne(id("listview"));
     list.children().each(function(child){
-        if(child && child.className().endsWith("RelativeLayout")){
+         if(child && child.className().endsWith("LinearLayout")){
             if(child.childCount() > 1){
-                sign(child);
+                sign(child.findOne(id("left_container")));
+                sign(child.findOne(id("right_container")));
             }
         }
     });
     list.scrollForward();
 }
-
-function sign(child){
-    var li = child.child(1);
-    if(li && li.childCount() > 0){
-        var name = li.child(0).text();
-        if(signed[name]){
-            return;
-        }
-        child.click();
-        sleep(400);
-        for(var i = 0; i < 3; i ++){
-          if(click("签到")){
-            break;
-          }
-          sleep(200);
-        }
-        sleep(400);
-        while(!back());
-        signed[name] = true;
+    
+function sign(c){
+  if(!c){
+    return;
+  }
+  var name = c.child(0).text();
+  if(signed[name]){
+     return;
+  }
+  c.click();
+  sleep(400);
+  for(var i = 0; i < 3; i ++){
+    if(click("签到")){
+       break;
     }
+    sleep(200);
+  }
+  sleep(600);
+  while(!back());
+  signed[name] = true;
 }
