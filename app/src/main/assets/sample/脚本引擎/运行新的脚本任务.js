37dc1966dc20a88d92e17c82e2a35025c1bf9020
hyb1996 <946994919@qq.com>
2017-08-04 16:35:29 +0800

    docs: engines module

--- app/src/main/assets/sample/脚本引擎/运行新的脚本任务.js
+++ app/src/main/assets/sample/脚本引擎/运行新的脚本任务.js
@@ -1,6 +1,6 @@
 var script = "toast('Hello, Auto.js');" +
              "sleep(3000);" +
              "toast('略略略');";
-var execution = engines.execScript(script);
+var execution = engines.execScript("Hello",  script);
 sleep(1000);
 execution.getEngine().forceStop();
