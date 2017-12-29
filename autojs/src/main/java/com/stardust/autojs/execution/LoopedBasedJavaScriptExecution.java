f39516a4b449482e938fef6f0ad538caec83be75
hyb1996 <946994919@qq.com>
2017-12-30 00:22:01 +0800

    fix: crash when a exception is thrown by script on ui thread

--- autojs/src/main/java/com/stardust/autojs/execution/LoopedBasedJavaScriptExecution.java
+++ autojs/src/main/java/com/stardust/autojs/execution/LoopedBasedJavaScriptExecution.java
@@ -23,6 +23,10 @@ public class LoopedBasedJavaScriptExecution extends RunnableScriptExecution {
         long delay = getConfig().delay;
         sleep(delay);
         final LoopBasedJavaScriptEngine javaScriptEngine = (LoopBasedJavaScriptEngine) engine;
+        javaScriptEngine.setUiThreadExceptionHandler((t, e) -> {
+            javaScriptEngine.forceStop();
+            getListener().onException(this, (Exception) e);
+        });
         final long interval = getConfig().interval;
         javaScriptEngine.getRuntime().loopers.setMainLooperQuitHandler(new Loopers.LooperQuitHandler() {
             long times = getConfig().loopTimes == 0 ? Integer.MAX_VALUE : getConfig().loopTimes;
