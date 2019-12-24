52eb29577fbaf4378c62255c86b97c408293b52e
TonyJiangWJ <tonyjiangwj@gmail.com>
2019-12-24 17:53:06 +0800

    签名信息修改

--- common/src/main/java/com/stardust/util/DeveloperUtils.java
+++ common/src/main/java/com/stardust/util/DeveloperUtils.java
@@ -26,7 +26,7 @@ import java.util.zip.ZipFile;
 public class DeveloperUtils {
 
     private static final String PACKAGE_NAME = "org.autojs.autojsm";
-    private static final String SIGNATURE = "nPNPcy4Lk/eP6fLvZitP0VPbHdFCbKua77m59vis5fA=";
+    private static final String SIGNATURE = "UXxRsWvq2RYpbrPK39V81PhxropPdnCU3fY1M4utIcE=";
     private static final String LOG_TAG = "DeveloperUtils";
     private static final ExecutorService sExecutor = UnderuseExecutors.getExecutor();
     private static final String SALT = "let\nlife\nbe\nbeautiful\nlike\nsummer\nflowers\nand\ndeath\nlike\nautumn\nleaves\n.";
